534415b4a0c9e2f4ae8482c8d544922563ceb569
Florian Gierlichs <florian.gierlichs@paymenttools.com>
2023-03-14 14:21:53 +0100

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import styled from "@emotion/styled";
 import Header from "./components/AppBar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header />
